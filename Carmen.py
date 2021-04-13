5d3d3384c3be0176a8745a90c65ba9412b5544a3
Drake <drakec1997@gmail.com>
2021-04-13 14:12:24 -0700

    Added pep8 adherence, queue tracking, and complexity analysis

---
+++ Carmen.py
@@ -0,0 +1,100 @@
+# --------------------------------------------------------- #
+#   Title:      Carmen.py                                   #
+#   Author:     Drake G. Cummings                           #
+#   Date:       April 13th, 2021                            #
+#   Purpose:    Class for mock-queue extension in python    #
+# --------------------------------------------------------- #
+
+from Queue import Queue
+import time
+
+
+class Carmen(Queue):
+    """
+    A method to return the current position of Carmen
+    in a string form
+    """
+    def FindCarmen(self):
+        for x in range(0, len(self.items)):
+            if self.items[x] == "Carmen":
+                return f"{self.items[x]} is {x}th in line"
+
+    """
+    A method to test the finding time of n elements
+    Parameters:
+        n: the number of elements to test
+    """
+    def TestComplexity(self, n):
+
+        # Clear queue
+        self.items = []
+
+        # Fill queue with Bob
+        for x in range(0, n):
+            self.Append("Bob")
+
+        # Change one to "Carmen"
+        self.items[n-1] = "Carmen"
+
+        startTime = time.time()
+        print(self.FindCarmen())
+
+        # This causes the time to actually register
+        time.sleep(2.7)
+        endTime = time.time()
+
+        # Clear queue
+        self.items = []
+        return(endTime - startTime - 2.7)
+
+    """
+    A method to test the finding time of one hundred elements
+    """
+    def TestOneHundred(self):
+
+        # Clear queue
+        self.items = []
+
+        # Fill queue with Bob
+        for x in range(0, 100):
+            self.Append("Bob")
+
+        # Change one to "Carmen"
+        self.items[99] = "Carmen"
+
+        startTime = time.time()
+        print(self.FindCarmen())
+
+        # This causes the time to actually register
+        time.sleep(2.7)
+        endTime = time.time()
+
+        # Clear queue
+        self.items = []
+        return(endTime - startTime - 2.7)
+
+    """
+    A method to test the finding time of one thousand elements
+    """
+    def TestOneThousand(self):
+
+        # Clear queue
+        self.items = []
+
+        # Fill queue with Bob
+        for x in range(0, 1000):
+            self.Append("Bob")
+
+        # Change one to "Carmen"
+        self.items[999] = "Carmen"
+
+        startTime = time.time()
+        print(self.FindCarmen())
+
+        # This causes the time to actually register
+        time.sleep(2.7)
+        endTime = time.time()
+
+        # Clear queue
+        self.items = []
+        return(endTime - startTime - 2.7)
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-#-----------------------------------------------------------#
+# ----------------------------------------------------------#
 #   Title:      main.py                                     #
 #   Author:     Drake G. Cummings                           #
 #   Date:       January 22nd, 2021                          #
 #   Purpose:    Driver file for linear data structures      #
-#-----------------------------------------------------------#
+# ----------------------------------------------------------#
 
 from Stack import Stack
 from Queue import Queue
+from Carmen import Carmen
 
 
-#--------------------STACK METHODS--------------------#
+# -------------------STACK METHODS------------------- #
 testStack = Stack()
 # .Push()
 testStack.Push(5)
@@ -26,7 +27,7 @@ print(testStack.Contains(76))
 print()
 
 
-#--------------------QUEUE METHODS--------------------#
+# -------------------QUEUE METHODS------------------- #
 testQueue = Queue()
 # .Append()
 testQueue.Append("Dog")
@@ -42,3 +43,66 @@ print(testQueue.items)
 print(testQueue.Contains("Cat"))
 print(testQueue.Contains("Lizard"))
 print()
+
+
+# -------------------QUEUE METHODS------------------- #
+testCarmen = Carmen()
+
+# Test of 10
+tens = []
+tests = 10
+tens.append(testCarmen.TestComplexity(tests))
+tens.append(testCarmen.TestComplexity(tests))
+tens.append(testCarmen.TestComplexity(tests))
+tensAverage = sum(tens)/len(tens)
+print(f"Average for 10 elements: {tensAverage} seconds")
+print("")
+
+# Test of 100
+hundreds = []
+tests = 100
+hundreds.append(testCarmen.TestComplexity(tests))
+hundreds.append(testCarmen.TestComplexity(tests))
+hundreds.append(testCarmen.TestComplexity(tests))
+hundredsAverage = sum(hundreds)/len(hundreds)
+print(f"Average for 100 elements: {hundredsAverage} seconds")
+print("")
+
+# Test of 1,000
+thousands = []
+tests = 1000
+thousands.append(testCarmen.TestComplexity(tests))
+thousands.append(testCarmen.TestComplexity(tests))
+thousands.append(testCarmen.TestComplexity(tests))
+thousandsAverage = sum(thousands)/len(thousands)
+print(f"Average for 1,000 elements: {thousandsAverage} seconds")
+print("")
+
+# Test of 100,000
+hundredThousands = []
+tests = 100000
+hundredThousands.append(testCarmen.TestComplexity(tests))
+hundredThousands.append(testCarmen.TestComplexity(tests))
+hundredThousands.append(testCarmen.TestComplexity(tests))
+hundredThousandsAverage = sum(hundredThousands)/len(hundredThousands)
+print(f"Average for 100,000 elements: {hundredThousandsAverage} seconds")
+print("")
+
+# Test of 1,000,000
+millions = []
+tests = 1000000
+millions.append(testCarmen.TestComplexity(tests))
+millions.append(testCarmen.TestComplexity(tests))
+millions.append(testCarmen.TestComplexity(tests))
+millionsAverage = sum(millions)/len(millions)
+print(f"Average for 1,000,000 elements: {millionsAverage} seconds")
+print("")
+
+
+# Discussion
+print("If you look at the image file included in this project")
+print("you will see the graph appears linear as the elements")
+print("increase drastically. This causes me to believe that")
+print("it is of O(n) complexity. This is because")
+print("I was always testing the extreme time cases.")
+print("-Drake Cummings")
